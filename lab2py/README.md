10f6968a6d7fd3cea550ddd1a66076e686ef280e
Agus sanjaya <agus.16092000@gmail.com>
2022-10-26 21:13:48 +0700

    tambah teks

---
+++ lab2py/README.md
@@ -0,0 +1,7 @@
+| Nama     | Agus sanjaya |
+| -------- | -----------  |
+| NIM      | 312010060    |
+| Kelas    | TI.20.A1     |
+| Matkul   | Kriptografi  |
+
+## Contoh perintah dasar python <br>
---
+++ lab2py/pyton.py
@@ -0,0 +1,11 @@
+# Mengambil input
+nama = input("Siapa nama kamu: ")
+umur = input("Berapa umur kamu: ")
+
+# Menampilkan output
+print("Hello",nama,"umur kamu adalah",umur,"tahun")
+a = 6
+b = 8
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a+b=",a+b)
